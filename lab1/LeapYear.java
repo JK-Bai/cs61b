88a5c84693c2296c22dfed4de2110482711145b6
Fang <ChaoHua@yuantsy.com>
2024-06-27 20:57:50 +0800

    Complete the modifications for Lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -5,7 +5,15 @@ public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
+     * 判断一个年份是否为闰年
      */
+    private static boolean isLeapYear(int year){
+        if((year%4==0!year%100!=0)||year%400==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
